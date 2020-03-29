f422562525b20550651c25cbf09058ecc7e86b86
nobu <algebra.zetafunc@gmail.com>
2020-03-29 11:03:07 +0900

    誤差が出ないようBigDecimalへ変更

--- src/section016/geometry/Operator.java
+++ src/section016/geometry/Operator.java
@@ -1,22 +1,39 @@
 package section016.geometry;
 
+import java.math.BigDecimal;
+
 // 演算クラス
 public class Operator {
 
 	public static Point add(Point p1, Point p2) {
-		return new Point(p1.x + p2.x, p1.y + p2.y);
+		BigDecimal p1x = new BigDecimal(Double.toString(p1.x));
+		BigDecimal p1y = new BigDecimal(Double.toString(p1.y));
+		BigDecimal p2x = new BigDecimal(Double.toString(p2.x));
+		BigDecimal p2y = new BigDecimal(Double.toString(p2.y));
+
+		return new Point(Double.valueOf(p1x.add(p2x).toPlainString()), Double.valueOf(p1y.add(p2y).toPlainString()));
 	}
 
 	public static Point sub(Point p1, Point p2) {
-		return new Point(p1.x - p2.x, p1.y - p2.y);
+		BigDecimal p1x = new BigDecimal(Double.toString(p1.x));
+		BigDecimal p1y = new BigDecimal(Double.toString(p1.y));
+		BigDecimal p2x = new BigDecimal(Double.toString(p2.x));
+		BigDecimal p2y = new BigDecimal(Double.toString(p2.y));
+
+		return new Point(Double.valueOf(p1x.subtract(p2x).toPlainString()), Double.valueOf(p1y.subtract(p2y).toPlainString()));
 	}
 
 	public static Point multi(Point p1, double k) {
-		return new Point(p1.x * k, p1.y * k);
+		BigDecimal _k = new BigDecimal(Double.toString(k));
+		BigDecimal p1y = new BigDecimal(Double.toString(p1.y));
+		return new Point(p1.x * k, Double.valueOf(p1y.multiply(_k).toPlainString()));
 	}
 
 	public static double norm(Point p) {
-		return p.x * p.x + p.y * p.y;
+		BigDecimal px = new BigDecimal(Double.toString(p.x));
+		BigDecimal py = new BigDecimal(Double.toString(p.y));
+
+		return Double.valueOf(px.multiply(px).add(py.multiply(py)).toPlainString());
 	}
 
 	public static double abs(Point p) {
--- src/section016/geometry/Point.java
+++ src/section016/geometry/Point.java
@@ -1,5 +1,7 @@
 package section016.geometry;
 
+import java.math.BigDecimal;
+
 // 点クラス
 public class Point {
 	public double x, y;
@@ -10,11 +12,21 @@ public class Point {
 	}
 
 	public static double dot(Point p1, Point p2) {
-		return p1.x * p2.x + p1.y * p2.y;
+		BigDecimal p1x = new BigDecimal(Double.toString(p1.x));
+		BigDecimal p1y = new BigDecimal(Double.toString(p1.y));
+		BigDecimal p2x = new BigDecimal(Double.toString(p2.x));
+		BigDecimal p2y = new BigDecimal(Double.toString(p2.y));
+
+		return Double.valueOf(p1x.multiply(p2x).add(p1y.multiply(p2y)).toPlainString());
 	}
 
 	public static double cross(Point p1, Point p2) {
-		return p1.x * p2.y - p1.y * p2.x;
+		BigDecimal p1x = new BigDecimal(Double.toString(p1.x));
+		BigDecimal p1y = new BigDecimal(Double.toString(p1.y));
+		BigDecimal p2x = new BigDecimal(Double.toString(p2.x));
+		BigDecimal p2y = new BigDecimal(Double.toString(p2.y));
+
+		return Double.valueOf(p1x.multiply(p2y).subtract(p1y.multiply(p2x)).toPlainString());
 	}
 
 }
